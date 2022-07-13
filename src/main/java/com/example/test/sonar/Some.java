613fcb3c967deb6db821f6ac755c760cb50828b0
yuraripetskyi <31436120+yuraripetskyi@users.noreply.github.com>
2022-07-13 20:31:28 +0300

    Update Some.java

--- src/main/java/com/example/test/sonar/Some.java
+++ src/main/java/com/example/test/sonar/Some.java
@@ -6,6 +6,28 @@ import java.math.BigDecimal;
 
 public class Some {
        
+       
+       private static int test876876() {
+       try {
+           while (true) {
+               BigDecimal bigDecimal = new BigDecimal(1);
+           }
+       } catch (Throwable throwable) {
+           System.out.println("123");
+       }
+       return 1;
+    }
+    
+     private static int test567567() {
+       try {
+           while (true) {
+               BigDecimal bigDecimal = new BigDecimal(2);
+           }
+       } catch (Throwable throwable) {
+           System.out.println("123");
+       }
+       return 1;
+    }
      
        public void smell4321231() {
         while (true) {
