fd57cf611e8b2e26d23e499eef98803cfbf93085
yuraripetskyi <31436120+yuraripetskyi@users.noreply.github.com>
2022-07-13 20:24:20 +0300

    Update Some.java

--- src/main/java/com/example/test/sonar/Some.java
+++ src/main/java/com/example/test/sonar/Some.java
@@ -6,7 +6,30 @@ import java.math.BigDecimal;
 
 public class Some {
        
-       
+     
+       public void smell4321231() {
+        while (true) {
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+            System.out.println(123);
+        }
+    }
+
+
+    private static int test1212343124() {
+       try {
+           while (true) {
+               BigDecimal bigDecimal = new BigDecimal(1);
+           }
+       } catch (Throwable throwable) {
+           System.out.println("123");
+       }
+       return 1;
+    }   
        
     public void smell4321() {
         while (true) {
